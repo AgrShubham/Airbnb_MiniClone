69de35e8eef838305b1573d927ba78744dfdae0e
Shubham Agrawal <ag.shubham747@gmail.com>
2024-01-10 12:37:02 +0530

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: "public",
-  base: "/Airbnb_MiniReactProject/"
+  base: "/Airbnb_MiniClone/"
 })
